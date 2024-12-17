ec616c932ddf9aa6984eabc10acf035a81239169
Joe Knowles <joejknowles@gmail.com>
2024-12-17 14:15:16 +0000

    Add token to http request headers

--- src/app/lib/graphql/apollo-client.ts
+++ src/app/lib/graphql/apollo-client.ts
@@ -1,13 +1,40 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+import { auth } from '@/lib/firebase';
 
 if (process.env.NODE_ENV === 'development') {
     loadDevMessages();
     loadErrorMessages();
 }
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
     uri: '/api/graphql',
+});
+
+const authLink = setContext(async (_, { headers }) => {
+    const user = auth.currentUser;
+
+    if (!user) {
+        return { headers };
+    }
+
+    try {
+        const token = await user.getIdToken();
+        return {
+            headers: {
+                ...headers,
+                Authorization: `Bearer ${token}`,
+            },
+        };
+    } catch (error) {
+        console.error('Error retrieving Firebase token:', error);
+        return { headers };
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
 
