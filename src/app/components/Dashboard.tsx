668c158b1ed1f5efc8e0811ad6871dfd48820a5c
Joe Knowles <joejknowles@gmail.com>
2024-12-29 12:12:56 +0000

    Mobile dashboard layout

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,43 +1,47 @@
-
-import { Box, Container } from "@mui/material";
-import { Tasks } from "./Tasks";
+import { Box, Container, useMediaQuery } from "@mui/material";
 import { DayGrid } from "./DayGrid";
+import { Tasks } from "./Tasks";
 import { withSignedInLayout } from "./SignedInLayout";
 
 const Dashboard = () => {
+    const isSmallScreen = useMediaQuery("(max-width:710px)");
+
     return (
         <Container sx={{ mt: 4 }}>
-            <Box
-                sx={{
-                    display: "flex",
-                    height: "80vh",
-                    justifyContent: "center",
-                }}
-            >
+            {isSmallScreen ? (
+                <Box>
+                    <Box sx={{ p: 2 }}>
+                        <DayGrid />
+                    </Box>
+                </Box>
+            ) : (
                 <Box
                     sx={{
-                        width: "250px",
-                        p: 2,
+                        display: "flex",
+                        height: "80vh",
+                        justifyContent: "center",
                     }}
                 >
-                    <Tasks />
+                    <Box sx={{ width: "250px", p: 2 }}>
+                        <Tasks />
+                    </Box>
+                    <Box
+                        sx={{
+                            width: "1px",
+                            bgcolor: "grey.200",
+                        }}
+                    />
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            p: 2,
+                            maxWidth: "400px",
+                        }}
+                    >
+                        <DayGrid />
+                    </Box>
                 </Box>
-                <Box
-                    sx={{
-                        width: "1px",
-                        bgcolor: "grey.200",
-                    }}
-                />
-                <Box
-                    sx={{
-                        flexGrow: 1,
-                        p: 2,
-                        maxWidth: "400px",
-                    }}
-                >
-                    <DayGrid />
-                </Box>
-            </Box>
+            )}
         </Container>
     );
 };
