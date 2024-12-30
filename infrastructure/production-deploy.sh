7fc65c2ef5fec1a6ba13657de62f8e59ff09f776
Joe Knowles <joejknowles@gmail.com>
2024-12-30 14:09:54 +0000

    Change directory if necessary in deploy

--- infrastructure/production-deploy.sh
+++ infrastructure/production-deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$(basename $(pwd))" != "pass-time" ]; then
+    cd "./pass-time" || {
+        echo "Error: Could not find or enter the 'pass-time' directory."
+        exit 1
+    }
+fi
+
 echo "Pulling latest code..."
 git pull origin main
 
