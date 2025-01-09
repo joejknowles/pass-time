c5b2857deddd4bf20d89bbb567fbf146dfa92c47
Joe Knowles <joejknowles@gmail.com>
2025-01-09 20:48:59 +0000

    Enabled suggestions

--- src/app/components/entityDetailsPanel/TaskDetailsSuggestions.tsx
+++ src/app/components/entityDetailsPanel/TaskDetailsSuggestions.tsx
@@ -1,10 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, FormControlLabel, Switch, Checkbox, FormGroup, Radio, RadioGroup } from "@mui/material";
+import { useState } from "react";
 
 export const TaskDetailsSuggestions = () => {
+    const [suggestionsEnabled, setSuggestionsEnabled] = useState(true);
+
     return (
         <Box>
-            <Typography variant="subtitle1">Suggestions / reminders</Typography>
-            {/* Add content for suggestions and reminders here */}
+            <FormControlLabel
+                control={
+                    <Switch
+                        checked={suggestionsEnabled}
+                        onChange={() => setSuggestionsEnabled(!suggestionsEnabled)}
+                    />
+                }
+                label={`Suggestions are ${suggestionsEnabled ? "enabled" : "disabled"}`}
+            />
         </Box>
     );
 };
