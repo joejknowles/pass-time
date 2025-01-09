b78b5accd19804e4936fefcac41f5ef733c321ac
Joe Knowles <joejknowles@gmail.com>
2025-01-09 12:06:52 +0000

    Add some indexes

---
+++ prisma/migrations/20250109120624_add_indexes_to_task/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "BalanceTarget_userId_idx" ON "BalanceTarget"("userId");
+
+-- CreateIndex
+CREATE INDEX "Task_userId_idx" ON "Task"("userId");
+
+-- CreateIndex
+CREATE INDEX "Task_title_idx" ON "Task"("title");
