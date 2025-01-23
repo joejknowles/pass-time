1c1113bd954d0da3468e1f793c3f11bc74dc36aa
Joe Knowles <joejknowles@gmail.com>
2025-01-23 18:19:33 +0000

    Add timestamps to every model

---
+++ prisma/migrations/20250123180805_add_timestamps/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `BalanceTarget` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Task` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `TaskInstance` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "BalanceTarget" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "TaskInstance" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
