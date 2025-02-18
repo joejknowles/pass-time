33456e2c340fb7a3f598e602f541c781582d5d5d
Joe Knowles <joejknowles@gmail.com>
2025-02-18 16:27:50 +0000

    Remove incorrect tracking call

--- src/app/components/Tracking.tsx
+++ src/app/components/Tracking.tsx
@@ -6,8 +6,6 @@ import { useEffect } from 'react';
 const siteId = 5308035;
 const hotjarVersion = 6;
 
-Hotjar.init(siteId, hotjarVersion);
-
 export const Tracking = () => {
     useEffect(() => {
         if (typeof window === 'undefined' || process.env.NODE_ENV === 'development') {
