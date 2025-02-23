50851cd9363cb2ce1d9874bf8b340a1070f10e97
Joe Knowles <joejknowles@gmail.com>
2025-02-23 15:43:56 +0000

    Fix panel padding

--- src/app/components/entityDetailsPanel/TaskDetails/TaskDetails.tsx
+++ src/app/components/entityDetailsPanel/TaskDetails/TaskDetails.tsx
@@ -50,7 +50,7 @@ export const TaskDetails = ({
                     backgroundColor: "white",
                     padding: 3,
                     width: "100%",
-                    height: "100%",
+                    minHeight: "100%",
                     position: "relative",
                     display: "flex",
                     flexDirection: "column",
